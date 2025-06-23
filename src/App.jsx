1ad37646095170ea59c651064ec04ae85a483331
Ufhano <4035709@myuwc.ac.za>
2025-06-23 10:11:19 +0200

    fragement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Message from "./message"
+import ListGroup from "./Components/ListGroup";
 
 function App(){
   return <div>
-    <Message>
-
-    </Message>
+    <ListGroup></ListGroup>
   </div>
 
 
