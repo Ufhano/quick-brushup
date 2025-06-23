914bd4fa8da12822473dbb5c3f6a340df2393e97
Ufhano <4035709@myuwc.ac.za>
2025-06-23 12:49:45 +0200

    Update ListGroup.tsx

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -4,7 +4,7 @@ import  {MouseEvent}from "react";
 function ListGroup() {
   let items = ["Johannesburg", "Malawi", "Thohoyandou", "Hayani"];
 
-
+//event handling the onclick
   const handleClick =(event:MouseEvent) => console.log(event)
 
   return (
@@ -17,7 +17,7 @@ function ListGroup() {
           <li
             className="list-group-item"
             key={item}
-            onClick={(event) => console.log(event)}
+            onClick={handleClick}
           >
             {" "}
             {item}
