0b90086e057d25a72ea472c2dff1061258e4605f
Ufhano <4035709@myuwc.ac.za>
2025-06-23 12:27:25 +0200

    Update ListGroup.tsx

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -2,17 +2,22 @@ import { Fragment } from "react";
 
 function ListGroup() {
   let items = ["Johannesburg", "Malawi", "Thohoyandou", "Hayani"];
-  items=[];
 
-  
   return (
     <>
       <h1>List</h1>
-      
-      {items.length===0 && <p>No items found</p>}
+
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}> {item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {" "}
+            {item}
+          </li>
         ))}
       </ul>
     </>
