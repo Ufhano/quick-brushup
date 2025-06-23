8180ba1ae4ab97408e6a5c839bdab558d9d4c0dc
Ufhano <4035709@myuwc.ac.za>
2025-06-23 12:20:23 +0200

    Update ListGroup.tsx

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -9,6 +9,7 @@ function ListGroup() {
     <>
       <h1>List</h1>
       {items.length ===0? <p>No Items found</p>:null}
+      {items.length===0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}> {item}</li>
