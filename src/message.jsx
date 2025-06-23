4cb5eeb3ac21b0164050a8f47d15ef0eb91e65d8
Ufhano <4035709@myuwc.ac.za>
2025-06-23 09:34:22 +0200

    Update message.jsx

--- src/message.jsx
+++ src/message.jsx
@@ -1,5 +1,8 @@
 //function based compents more consise 
 function Message(){
+    const name='Ufhano';
+    if (name)
+        return<h1>Hello {name}</h1>;
     return <h1> hi, first brsuh up  </h1>;
 }
 export default Message;
