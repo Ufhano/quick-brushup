d249140656b7b9d112de0e4824bd6963f2562714
Ufhano <4035709@myuwc.ac.za>
2025-06-24 16:06:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 
-
-function App(){
-
-
-  return( <div> 
-    <Alert/>
-  
-  </div>
-
+import Alert from "./Components/Alert";
+function App() {
+  return (
+    <div>
+      <Alert />
+    </div>
   );
 }
 export default App;
