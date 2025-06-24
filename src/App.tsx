a299ae475db0a120a28654aff290239c93882c1b
Ufhano <4035709@myuwc.ac.za>
2025-06-24 15:48:53 +0200

    naming issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import ListGroup from "./Components/ListGroup";
 
 function App(){
+
+  const handleSelectionItem =(item:string)=>{
+    console.log(item);
+  }
   let items = ["Johannesburg", "Malawi", "Thohoyandou", "Hayani"];
   return <div>
-    <ListGroup items ={items}heading="Cities" onSelectionItem={2}/>
+    <ListGroup items ={items}heading="Cities" onSelectionItem={handleSelectionItem}/>
     
   </div>
 
